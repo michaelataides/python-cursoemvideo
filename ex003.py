cfe4dcfb1271add5caac164450bd45172a7503af
Michael Ataides de Melo <michaelataides@gmail.com>
2020-04-16 00:01:49 -0300

    novoexercio03

---
+++ ex003.py
@@ -0,0 +1,8 @@
+num0 = int(input('Digite um valor: '))
+num1 = int(input('Digite outro valor: '))
+msg = 'Hello'
+print(msg,num0,'e', num1)
+msg2 = 'A soma é'
+print(msg2,':', num0 + num1)
+soma = num0 + num1
+print('A soma entre {} e {} é igual a {}!'.format(num0,num1,soma))
